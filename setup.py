e15c5f0bc86fa7ec8e54616438154aba1a085e43
Satoshi <88019950+superleesa@users.noreply.github.com>
2024-03-21 19:37:04 +0800

    Add co-authors in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='toenet',
     version='0.1',
     url='https://github.com/YuanGao-YG/TOENet',
-    author='Yuan Gao',
+    author='Yuan Gao, Wenyu Xu, and Yuxu Lu',
     author_email='author@gmail.com',
     description='Python implementation of the paper: Let You See in Haze and Sandstorm: Two-in-One Low-visibility Enhancement Network',
     packages=find_packages(),    
