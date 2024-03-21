d9d4f3bb78f6d65ca816914959eb63130b688cf4
Satoshi <88019950+superleesa@users.noreply.github.com>
2024-03-21 18:58:43 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TOENet',
+    version='0.1',
+    url='https://github.com/YuanGao-YG/TOENet',
+    author='Yuan Gao',
+    author_email='author@gmail.com',
+    description='Python implementation of the paper: Let You See in Haze and Sandstorm: Two-in-One Low-visibility Enhancement Network',
+    packages=find_packages(),    
+    install_requires=[],
+)
